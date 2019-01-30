c44d8c89ab1f2d4684a899b661ca79e0af9d253d
TrevorVieth <35700988+TrevorVieth@users.noreply.github.com>
2019-01-30 16:15:09 -0600

    InClass Stuff 1/30/19

---
+++ Software-Sys-Dev/SimpleDepedencyInClass/MessagePrinter/Class1.cs
@@ -0,0 +1,24 @@
+using System;
+namespace MessagePrinter
+{
+    public class MessagePrintingService
+    {
+
+        public string Message{get; set;} = "Default";
+
+        public MessagePrintingService()
+        {
+
+        }
+
+        public MessagePrintingService(string message)
+        {
+            this.Message = message;
+        }
+        
+        public void PrintMessage()
+        {
+            Console.WriteLine(this.Message);
+        }
+    }
+}
